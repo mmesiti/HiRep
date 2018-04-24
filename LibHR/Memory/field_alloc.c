12303f62e9af80d3e6bdeb9192fb8ff9f8c7b732
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2018-04-24 17:03:57 +0100

    Fix data stucture used to store the clover field with symplectic fermion matrices

--- LibHR/Memory/field_alloc.c
+++ LibHR/Memory/field_alloc.c
@@ -126,5 +126,9 @@ _DECLARE_MEMORY_FUNC(avfield, suNg_av_field, 4);
 _DECLARE_MEMORY_FUNC(gtransf, suNg_field, 1);
 
 _DECLARE_MEMORY_FUNC(clover_ldl, ldl_field, 1);
+#if defined(GAUGE_SPN) && defined(REPR_FUNDAMENTAL)
+_DECLARE_MEMORY_FUNC(clover_term, suNffull_field, 4);
+#else
 _DECLARE_MEMORY_FUNC(clover_term, suNfc_field, 4);
+#endif
 _DECLARE_MEMORY_FUNC(clover_force, suNf_field, 6);
