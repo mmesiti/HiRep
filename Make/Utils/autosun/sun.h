48cdaa576ffa2a0702db176d7780aa7150191780
Michele Mesiti <michele.mesiti@gmail.com>
2018-04-28 18:58:29 +0100

    Possible fix to symplecticity written and tested

--- Make/Utils/autosun/sun.h
+++ Make/Utils/autosun/sun.h
@@ -1,6 +1,8 @@
 #ifndef SUN_H
 #define SUN_H
 #include "./matrix.h"
+#include <assert.h>
+#include <iostream>
 
 string infinitesimal_evolution(const char* vname, const char* hname, const char* uname, const char* dtname);
 string fundamental_algebra_represent(const char* mname, const char* hname);
@@ -210,7 +212,171 @@ string infinitesimal_evolution(const char* vname, const char* hname, const char*
     return RET;
 }
 
+#ifdef _GAUGE_SPN_
+static inline void handle_subgroup(pmatrix& M, ostringstream& RET, 
+        int i, int j,int k, const char* uname){
+    polynomial tmp;
+    pconstant ntmp(1.0/group::N);
+    tmp = M.get(j,j);
+    tmp.minus();
+    tmp += M.get(i,i);
+    tmp *= ntmp;
+    RET <<
+        "\ty[0] = " << M.get(i,j).str_imag() << ";\n" <<
+        "\ty[1] = " << M.get(i,j).str_real() << ";\n" <<
+        "\ty[2] = " << tmp.str_imag() << ";\n" <<
+        "\tYtoU(s[" << k << "],y);\n";
+    for(int p = 0; p < group::N; p++)
+        RET << "\tsu2_rotate(s[" << k << "],&("
+            << uname << mindex(i,p,group::N) << "),&("
+            << uname << mindex(j,p,group::N) << "));\n";
 
+}
+
+
+string ExpX(const char* dtname,  const char* hname, const char* uname)
+{
+    ostringstream RET;
+
+    rvector H(group::DIM,hname);
+    pmatrix M(group::N);
+    rvariable dt(dtname);
+
+    dt.scale(complex(0.0,1.0));
+    H.scale(dt);
+
+    for(int A = 0; A < group::DIM; A++)
+    {
+        pmatrix T(group::T[A]);
+        T.scale(H.get(A));
+        M.add(T);
+    }
+
+    RET << 
+        "\tdouble y[3];\n" << 
+        "\tdouble s[" << group::N*(group::N-1)/2 << "][4];\n";
+
+    RET <<
+        "\tsuNgfull ut, *u;\n\n"
+        "\t_suNg_expand(ut,*r);\n"
+        "\tu=&ut;\n\n";
+	
+/*  In order to preserve symplecticity, we need to perform the 
+ *  multiplications in a specific order. SPN generators in the form
+ *  used here act on up to 2 different SU(2) subgroups (which commute 
+ *  which each other) at the same time.
+ *  SU(2) subgroups are identified by the two rows (or, equivalently,
+ *  columns) they act upon. 
+ *  Generators can be written as 
+ *   
+ *   A   B
+ *   B* -A*
+ *
+ *  In the following example (the SP(6) case) the following notation
+ *  is used:
+ *  - '-': subgroup does not exist ( i>j )
+ *  - 'A': subgroup in the 'A' sector of the matrix
+ *  - 'a': subgroup in the '-A*' sector of the matrix which must be 
+ *         taken care of at the same time as the corresponding subgroup 
+ *         in the 'A' sector if the matrix (it actually commutes with it).
+ *         Given an A subgroup (i,j), the corresponding 'a' subgroup
+ *         is (i+N/2,j+N/2).
+ *  - '1': subgroup in the [B+B*+diagonal] part of the matrix 
+ *         which has NO corresponding subgroup
+ *  - 'B': subgroup in the [B+B*+diagonal] part of the matrix 
+ *         which has ONE corresponding subgroup
+ *  - 'b': subgroup in the [B+B*+diagonal] part of the matrix 
+ *         which has ONE corresponding subgroup marked as 'B', and which
+ *         must be taken care of at the same time as it.
+ *         Given a B subgroup (i,j), the corresponding 'b' subgroup
+ *         is obtained reflecting along the diagonal of the 'B' submatrix.
+ *         that is  ((3*N/2-i)-1, (N/2-j)-1)
+ *  
+ *       i=
+ *       012345
+ *  j=0  -AA1bb
+ *    1  --AB1b
+ *    2  ---BB1
+ *    3  ----aa 
+ *    4  -----a
+ *    5  ------
+ *    
+ * *************************/
+
+    int k = 0;
+    for(int jt = 0; jt < group::N/2; jt++) 
+        for(int it = jt+1; it <= jt + group::N/2; it++)
+        {
+            RET << "// " << it << " " << jt << "\n";
+            {
+                //taking care of the it,jt SU(2) subgroup
+                int i = it, j = jt;
+                handle_subgroup(M,RET,i,j,k,uname);
+                k++;
+            }
+            if(it<group::N/2 && jt < group::N/2){
+                // we are in the 'A' part of the algebra matrix. 
+                // we need to take care of the it+N/2,jt+N/2 SU(2) subgroup
+                int i = it+group::N/2, j = jt+group::N/2;
+                handle_subgroup(M,RET,i,j,k,uname);
+                k++;
+            }else if(it>=group::N/2 && it != jt+group::N/2){
+                // we are in the 'B' part of the algebra matrix
+                // and we need to take care of the other SU2 subgroup
+                // transposing i and j w.r.t the diagonal of the B
+                // part of the matrix
+                int i = group::N/2+jt, j = it-group::N/2;
+                handle_subgroup(M,RET,i,j,k,uname);
+                k++;
+            }
+            else assert(it == jt + group::N/2);
+        }
+
+    assert(k == group::N*(group::N-1)/2);
+    k--;
+    for(int jt = group::N/2-1; jt >= 0 ; --jt) 
+        for(int it = jt + group::N/2; it >= jt + 1; --it)
+        {
+            RET << "// " << it << " " << jt << "\n";
+            if(it<group::N/2 && jt < group::N/2){
+                // we are in the 'A' part of the algebra matrix. 
+                // we need to take care of the it+N/2,jt+N/2 SU(2) subgroup
+                int i = it+group::N/2, j = jt+group::N/2;
+                for(int p = 0; p < group::N; p++)
+                    RET << "\tsu2_rotate(s[" << k << "],&("
+                        << uname << mindex(i,p,group::N) << "),&("
+                        << uname << mindex(j,p,group::N) << "));\n";
+                k--;
+            }else if(it>=group::N/2 && it != jt+group::N/2){
+                // we are in the 'B' part of the algebra matrix
+                // and we need to take care of the other SU2 subgroup
+                /// transposing i and j w.r.t the diagonal of the B
+                // part of the matrix
+                int i = group::N/2+jt, j = it-group::N/2;
+                for(int p = 0; p < group::N; p++)
+                    RET << "\tsu2_rotate(s[" << k << "],&("
+                        << uname << mindex(i,p,group::N) << "),&("
+                        << uname << mindex(j,p,group::N) << "));\n";
+                k--;
+            }
+            else assert(it == jt + group::N/2);
+            {
+                //taking care of the it,jt SU(2) subgroup
+                int i = it, j = jt;
+                for(int p = 0; p < group::N; p++)
+                    RET << "\tsu2_rotate(s[" << k << "],&("
+                        << uname << mindex(i,p,group::N) << "),&("
+                        << uname << mindex(j,p,group::N) << "));\n";
+                k--;
+            }
+        }
+    assert(k == -1);
+
+    RET <<
+        "\n\tfor (int i=0; i<NG*NG/2; ++i) { r->c[i].re=ut.c[i].re; r->c[i].im=ut.c[i].im; }\n";
+    return RET.str();
+}
+#else
 string ExpX(const char* dtname,  const char* hname, const char* uname)
 {
     ostringstream RET;
@@ -241,20 +407,6 @@ string ExpX(const char* dtname,  const char* hname, const char* uname)
         "\tu=&ut;\n\n";
 #endif
 
-#ifdef _GAUGE_SPN_
-    RET <<
-        "\tsuNgfull ut, *u;\n\n"
-        "\tfor (int i=0; i<NG*NG/2; ++i) { ut.c[i].re=r->c[i].re; ut.c[i].im=r->c[i].im; }\n"
-        "\tfor (int i=0; i<NG/2; ++i) {\n"
-        "\t\tfor (int j=0; j<NG/2; ++j) {\n"
-        "\t\t\tint ind = NG*i+j;\n"
-        "\t\t\tut.c[NG*NG/2+NG/2+ind].re=r->c[ind].re; ut.c[NG*NG/2+NG/2+ind].im=-r->c[ind].im;\n"
-        "\t\t\tut.c[NG*NG/2+ind].re=-r->c[ind+NG/2].re; ut.c[NG*NG/2+ind].im=r->c[ind+NG/2].im;\n"
-        "\t\t}\n"
-        "\t}\n"
-        "\tu=&ut;\n\n";
-#endif
-	
     int k = 0;
     for(int j = 1; j < group::N; j++)
         for(int i = 0; i < j; i++)
@@ -290,14 +442,10 @@ string ExpX(const char* dtname,  const char* hname, const char* uname)
 #ifdef _GAUGE_SON_
     RET<<"\n\tfor (int i=0; i<NG*NG; ++i) { r->c[i]=ut.c[i].re; }\n";
 #endif
-#ifdef _GAUGE_SPN_
-    RET <<
-        "\n\tfor (int i=0; i<NG*NG/2; ++i) { r->c[i].re=ut.c[i].re; r->c[i].im=ut.c[i].im; }\n";
-#endif
 
     return RET.str();
 }
-
+#endif
 
 string fundamental_algebra_represent(const char* mname, const char* hname)
 {
